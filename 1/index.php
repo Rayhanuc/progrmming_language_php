2c710b4de074a01dd47e1497e89333ece28023ee
Rayhanuc <rayhanctg4@gmail.com>
2023-05-13 20:57:23 +0600

    Programming Language PHP

---
+++ 1/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Basic PHP</title>
+
+        <!-- bootstrap css link -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <!-- font-awesome css link -->
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+        <!-- style css link -->
+        <link href="assets/css/style.css" rel="stylesheet">
+        <!-- responsive css link -->
+        <link href="assets/css/responsive.css" rel="stylesheet">
+        <!-- css link end -->
+        
+    </head>
+    <body>
+        
+        <div class="content-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php
+                        echo "<h1 class='text-center'>Programming Language PHP</h1> \n<br/>";
+                        echo "<h3 class='text-center'>PHP Basics – Introduction to PHP and How to Write PHP Code – Part 1 & 2</h3> \n<br/>";
+                        ?>
+                        <hr>
+                        <?php echo "<h4>PHP Code: </h4>"; ?>
+                        <pre>
+
+echo "Hello this is php code from 2023"
+
+                        </pre>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="content-area code-result-area">
+
+                        <?php
+                        echo "<h4>PHP Code Result: </h4>";
+                        
+// php code start
+
+echo "Hello this is php code from 2023"
+
+// php end
+                        ?>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="copyright-area-text">
+                        <p ><?php echo "© 2023 -  <a  target='_blank' href='https://rayhanuddinchy.com/'>Rayhan Uddin Chowdhury</a>" ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
+        <!-- jquery js link -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!-- bootstrap js link -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <!-- main js link -->
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Programming Language PHP</title>
+
+    <!-- bootstrap css link -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- font-awesome css link -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <!-- style css link -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- responsive css link -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <!-- css link end -->
+
+</head>
+<body>
+    <div class="heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1><?php echo "Programming Language PHP" ?></h1>
+                    <p><?php echo "Click on the class name to see the code and result." ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="header-menu">
+                    <ul>
+                        <!-- Chapter 1 link started from here -->
+                        <!-- <h1 class="chapter"><?php echo "27 - MySQL Extra"; ?></h1> -->
+                        <li><a href="1/index.php" target="_blank">
+                                <?php echo "PHP Basics – Introduction to PHP and How to Write PHP Code – Part 1 & 2"; ?>
+                            </a>
+                        </li>
+
+
+
+                        
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="copyright-area-text">
+                        <p ><?php echo "© 2023 -  <a  target='_blank' href='https://rayhanuddinchy.com/'>Rayhan Uddin Chowdhury</a>" ?></p>
+                        <a href=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- jquery js link -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- bootstrap js link -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- main js link -->
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
