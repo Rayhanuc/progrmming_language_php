bd6223e7725114d28ebbb7b32bf8dc5eb4ac1a6f
Rayhanuc <rayhanctg4@gmail.com>
2023-05-26 19:21:46 +0600

    PHP Basics – Introduction and Usage of Internal Functions – Part 15

--- 15/index.php
+++ 15/index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>PHP - What are Functions and Why Use Functions</title>
+        <title>PHP - Introduction and Usage of Internal Functions</title>
 
         <!-- bootstrap css link -->
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
@@ -32,61 +32,31 @@
                     <div class="col-md-12">
                         <?php
                         echo "<h1 class='text-center'>Programming Language PHP</h1> \n<br/>";
-                        echo "<h3 class='text-center'>PHP Basics – What are Functions and Why Use Functions – Part 14</h3> \n<br/>";
+                        echo "<h3 class='text-center'>PHP Basics – Introduction and Usage of Internal Functions – Part 15</h3> \n<br/>";
                         ?>
                         <hr>
                         <?php echo "<h4>PHP Code: </h4>"; ?>
                         <pre>
 <code>
 
-echo "<h3>Normal code writing</h3>";
-$number1 = 10;
-$number2 = 20;
+echo "<h3>Array</h3>";
+$array = [
+    20, 10, 40, 55, 59, 50, 60, 70, 90, 40, 55, 100, 10
+];
+echo '<h5>Before filter total value : '. count($array). '</h5>';
+$filtered_array = array_unique($array);
 
-echo 'Result : ' . $number1 + $number2 . '<br>';
-
-
-$number3 = 25;
-$number4 = 10;
-
-echo 'Result : ' . $number3 + $number4 . '<br>';
+var_dump($filtered_array.count);
+echo '<br>';
+echo '<h5>After filter total value : '. count($filtered_array). '</h5>';
+echo '<pre>';
+var_dump($filtered_array);
+echo '</pre>';
+echo '<br>';
+echo '<pre>';
+print_r($filtered_array);
+echo '</pre>';
 echo "===========<br>";
-
-echo "<h3>User-defined functions</h3>";
-
-
-function functionName() {
-    echo 'Hello! I am from functionName.';
-}
-
-functionName();
-echo "<br>";
-echo "===========<br>";
-
-function sum($n, $m) {
-    echo $n + $m;
-}
-sum(57, 93);
-echo "<br>";
-sum(278, 27);
-echo "<br>";
-
-echo "===========<br>";
-
-function total($l, $k) {
-    return $l + $k;
-}
-
-echo total(36, 81);
-echo "<br>";
-
-echo total(36, 81) + 59;
-echo "<br>";
-
-echo total(500, 200) - 36;
-echo "<br>";
-
-echo total(50, 20) * 36;
 
 </code>
                         </pre>
@@ -105,55 +75,24 @@ echo total(50, 20) * 36;
 // php code start
 echo "<hr>";
 
-echo "<h3>Normal code writing</h3>";
-$number1 = 10;
-$number2 = 20;
+echo "<h3>Array</h3>";
+$array = [
+    20, 10, 40, 55, 59, 50, 60, 70, 90, 40, 55, 100, 10
+];
+echo '<h5>Before filter total value : '. count($array). '</h5>';
+$filtered_array = array_unique($array);
 
-echo 'Result : ' . $number1 + $number2 . '<br>';
-
-
-$number3 = 25;
-$number4 = 10;
-
-echo 'Result : ' . $number3 + $number4 . '<br>';
+var_dump($filtered_array.count);
+echo '<br>';
+echo '<h5>After filter total value : '. count($filtered_array). '</h5>';
+echo '<pre>';
+var_dump($filtered_array);
+echo '</pre>';
+echo '<br>';
+echo '<pre>';
+print_r($filtered_array);
+echo '</pre>';
 echo "===========<br>";
-
-echo "<h3>User-defined functions</h3>";
-
-
-function functionName() {
-    echo 'Hello! I am from functionName.';
-}
-
-functionName();
-echo "<br>";
-echo "===========<br>";
-
-function sum($n, $m) {
-    echo $n + $m;
-}
-sum(57, 93);
-echo "<br>";
-sum(278, 27);
-echo "<br>";
-
-echo "===========<br>";
-
-function total($l, $k) {
-    return $l + $k;
-}
-
-echo total(36, 81);
-echo "<br>";
-
-echo total(36, 81) + 59;
-echo "<br>";
-
-echo total(500, 200) - 36;
-echo "<br>";
-
-echo total(50, 20) * 36;
-
 
 
 // php end
